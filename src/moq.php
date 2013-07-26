4159ffdf2718969a5c8913b2b7a0cd0ac60411fc
Diego Caponera <diego.caponera@gmail.com>
2013-07-26 16:25:32 +0200

    Recursive str replace.

--- src/moq.php
+++ src/moq.php
@@ -11,6 +11,67 @@
 require __DIR__ . "/spyc.php";
 
 /**
+ * Recursive alternative to str_replace that supports replacing keys as well
+ *
+ * The following code block can be utilized by PEAR's Testing_DocTest
+ * <code>
+ * // Input //
+ * $settings = array(
+ *    "Credits" => "@appname@ created by @author@",
+ *    "Description" => "@appname@ can parse logfiles and store then in mysql",
+ *    "@author@_mail" => "kevin@vanzonneveld.net"
+ * );
+ * $mapping = array(
+ *    "@author@" => "kevin",
+ *    "@appname@" => "logchopper"
+ * );
+ *
+ * // Execute //
+ * $settings = replaceTree(
+ *    array_keys($mapping), array_values($mapping), $settings, true
+ * );
+ *
+ * // Show //
+ * print_r($settings);
+ *
+ * // expects:
+ * // Array
+ * // (
+ * //     [Credits] => logchopper created by kevin
+ * //     [Description] => logchopper can parse logfiles and store then in mysql
+ * //     [kevin_mail] => kevin@vanzonneveld.net
+ * // )
+ * </code>
+ *
+ * @param string  $search
+ * @param string  $replace
+ * @param array   $array
+ * @param boolean $keys_too
+ *
+ * @return array
+ */
+function replaceTree($search="", $replace="", $array=false, $keys_too=false)
+{
+  if (!is_array($array)) {
+      // Regular replace
+      return str_replace($search, $replace, $array);
+  }
+
+  $newArr = array();
+  foreach ($array as $k=>$v) {
+      // Replace keys as well?
+      $add_key = $k;
+      if ($keys_too) {
+          $add_key = str_replace($search, $replace, $k);
+      }
+
+      // Recurse
+      $newArr[$add_key] = replaceTree($search, $replace, $v, $keys_too);
+  }
+  return $newArr;
+}
+
+/**
 *	Route Class - wraps status-based HTTP respones.
 */
 class Route
@@ -126,14 +187,14 @@ class Route
 		foreach($urlTokens as $token)
 		{
 
-			strpos($token, ":") === 0 && $this -> responses[$status ?: $keys[0]] = str_replace(
+			strpos($token, ":") === 0 && $this -> responses[$status ?: $keys[0]] = replaceTree(
 				$token,
 				$matches[$i++],
 				$this -> responses[$status ?: $keys[0]]
 			);
 
 		}
-
+    
 		return true;
 
 	}
